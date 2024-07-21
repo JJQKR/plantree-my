d8f8ba0a2f5068cd06d93c152839079b29e8dce6
Boyoung Lee <genuss040822@gamil.com>
2024-07-21 14:47:07 +0900

    add: 수파베이스 연결, tanstack query 설정 변경

--- src/providers/query.provider.tsx
+++ src/providers/query.provider.tsx
@@ -1,10 +1,20 @@
+'use client';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { PropsWithChildren } from 'react';
+import { useState } from 'react';
 
-const queryClient = new QueryClient();
-
-const QueryProvider = ({ children }: PropsWithChildren) => {
+export const ReactQueryClientProvider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000
+          }
+        }
+      })
+  );
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
-
-export default QueryProvider;
