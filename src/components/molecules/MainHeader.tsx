09b7ba460759fc43cb8f4b31448ee848231b423f
youngsupark1 <eddy364885@gmail.com>
2024-07-22 12:20:11 +0900

    테스트

--- src/components/molecules/MainHeader.tsx
+++ src/components/molecules/MainHeader.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const MainHeader = () => {
-  return <div>MainHeader</div>;
+  return (
+    <div>
+      MainHeader
+      <div>
+        <p>테스트!</p>
+      </div>
+    </div>
+  );
 };
 
 export default MainHeader;
