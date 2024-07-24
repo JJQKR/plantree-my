05ffa635bc12bbeb24caa0df67a561cba5989e16
youngsupark1 <eddy364885@gmail.com>
2024-07-24 20:41:44 +0900

    ✨ update: 그리드 전환 버튼 위치 조정

--- src/components/atoms/GridToggleButton.tsx
+++ src/components/atoms/GridToggleButton.tsx
@@ -6,7 +6,7 @@ const GridToggleButton: React.FC<GridToggleButtonProps> = ({ onClick, gridView }
   return (
     <button
       onClick={onClick}
-      className="p-2 bg-transparent border-none outline-none hover:bg-gray-700 rounded transition duration-300"
+      className="p-2 mr-4 bg-transparent border-none outline-none hover:bg-gray-700 rounded transition duration-300"
     >
       {gridView ? (
         <AiFillBook className="text-2xl" /> // gridView가 true일 때 AiFillBook 아이콘 렌더링
