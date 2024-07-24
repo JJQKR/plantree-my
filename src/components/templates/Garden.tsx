dccee9bdc882282c501ee21e5de881a09515e19b
JJQKR <yjcoyjco@naver.com>
2024-07-24 16:18:20 +0900

    [  add ] Garden.tsx 파일과 BadgeCollection.tsx파일 생성

--- src/components/templates/Garden.tsx
+++ src/components/templates/Garden.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from 'react';
 
 const Garden = ({ children }: PropsWithChildren) => {
-  return <div className="bg-[#1edaaa] w-[200px] h-[100px]">{children}</div>;
+  return <div className="bg-[#1edaaa] w-[300px] h-[100px]">{children}</div>;
 };
 
 export default Garden;
