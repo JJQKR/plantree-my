b2b8ea35349e48fad04f5de6d92c9019da89d3a9
JJQKR <yjcoyjco@naver.com>
2024-07-25 10:08:08 +0900

    [chore]

--- src/components/molecules/NicknameModal.tsx
+++ src/components/molecules/NicknameModal.tsx
@@ -1,11 +1,20 @@
 import useMyModalStore from '@/stores/my.modal.store';
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 
 const NicknameModal: React.FC = () => {
   const { isNicknameModalOpen, toggleNicknameModal } = useMyModalStore((state) => state);
-  const [nickname, setNickname] = useState('');
+  const nicknameRef = useRef<HTMLInputElement>(null);
+
   const handleBackGroundClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (e.target === e.currentTarget) {
+      toggleNicknameModal();
+    }
+  };
+
+  const handleNicknameSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (nicknameRef.current) {
+      const newNickname = nicknameRef.current.value;
       toggleNicknameModal();
     }
   };
@@ -17,32 +26,32 @@ const NicknameModal: React.FC = () => {
         onClick={handleBackGroundClick}
       >
         <div className="bg-white p-4 rounded">
-          <div className="flex flex-col-3 jutify-between">
-            <div onClick={(e) => e.stopPropagation()}>
-              <h1 className="text-xl font-bold mb-4 text-center text-emerald-400">닉네임 변경하기</h1>
-
+          <div onClick={(e) => e.stopPropagation()}>
+            <h1 className="text-xl font-bold mb-4 text-left text-emerald-400">닉네임 변경하기</h1>
+            <form className="flex flex-col gap-2" onSubmit={handleNicknameSubmit}>
               <input
                 type="text"
                 placeholder="새 닉네임 입력"
                 className="mb-4 p-2 border rounded w-full text-black"
-                value={nickname}
-                onChange={(e) => setNickname(e.target.value)}
+                ref={nicknameRef}
               />
+
+              <div className="flex flex-col gap-2">
+                <button
+                  type="submit"
+                  className="px-4 py-2 bg-red-400 w-full text-white rounded"
+                  // onSubmit={toggleNicknameModal}
+                >
+                  저장
+                </button>
+              </div>
+
               <div className="flex flex-col gap-2">
                 <button className="px-4 py-2 bg-red-400 w-full text-white rounded" onClick={toggleNicknameModal}>
                   취소
                 </button>
-                <form>
-                  <button
-                    type="submit"
-                    className="px-4 py-2 bg-red-400 w-full text-white rounded"
-                    onSubmit={toggleNicknameModal}
-                  >
-                    저장
-                  </button>
-                </form>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
