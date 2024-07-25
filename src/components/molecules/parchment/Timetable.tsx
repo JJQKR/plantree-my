3914de8ff79c6101480cba00eb35875aaf67149c
Boyoung Lee <genuss040822@gamil.com>
2024-07-26 00:54:38 +0900

    [♻️ refactor] 10분 캘린더 타임테이블 부분 하드코딩에서 map으로 변경

--- src/components/molecules/parchment/Timetable.tsx
+++ src/components/molecules/parchment/Timetable.tsx
@@ -3,28 +3,32 @@
 import React, { useState } from 'react';
 
 const Timetable = () => {
-  const [color, setColor] = useState('');
+  // const [color, setColor] = useState('');
 
   const rows = 24;
   const columns = 6;
-  const colors = ['bg-red-200', 'bg-green-200', 'bg-blue-200', 'bg-yellow-200', 'bg-purple-200', 'bg-pink-200'];
+  const minutes = [10, 20, 30, 40, 50, 60];
+  const hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 
   return (
-    <div className="border">
+    <div className="border-2 relative h-full">
       <h4>Time Table</h4>
-      <div className="flex flex-row gap-4">
-        <span>10</span> <span>20</span>
-        <span>30</span>
-        <span>40</span>
-        <span>50</span>
-        <span>60</span>
+      <div className="flex flex-row gap-4 absolute right-1">
+        {minutes.map((minute) => {
+          return <div key={minute}>{minute}</div>;
+        })}
       </div>
-      <table className="w-5/6 h-full border-collapse">
+      <div className="flex flex-col absolute top-12 text-[9px]">
+        {hours.map((hour) => {
+          return <div key={hour}>{hour}</div>;
+        })}
+      </div>
+      <table className="w-5/6 h-72 border-collapse absolute top-12 right-1">
         <tbody>
           {Array.from({ length: rows }, (_, rowIndex) => (
             <tr key={rowIndex}>
               {Array.from({ length: columns }, (_, colIndex) => (
-                <td key={colIndex} className={`border border-gray-300 p-1.5 text-center bg-[${color}]`}>
+                <td key={colIndex} className={`border border-gray-300 p-1.5 text-center`}>
                   {/* Row {rowIndex + 1}, Col {colIndex + 1} */}
                 </td>
               ))}
