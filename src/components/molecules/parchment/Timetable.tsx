b2f5a3c8e991737594962b2ec27d0a98c43d2156
Boyoung Lee <genuss040822@gamil.com>
2024-07-26 11:42:52 +0900

    [✨ update ] 타임테이블 액티브 기능 추가

--- src/components/molecules/parchment/Timetable.tsx
+++ src/components/molecules/parchment/Timetable.tsx
@@ -2,13 +2,26 @@
 
 import React, { useState } from 'react';
 
+type activeCellsObjet = {
+  [key: string]: boolean;
+};
+
 const Timetable = () => {
-  // const [color, setColor] = useState('');
+  const [activeCells, setActiveCells] = useState<activeCellsObjet>({});
 
   const rows = 24;
   const columns = 6;
   const minutes = [10, 20, 30, 40, 50, 60];
   const hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+
+  const toggleCellColor = (id: string) => {
+    const changeColor = (prev: activeCellsObjet) => {
+      return { ...prev, [id]: !prev[id] };
+    };
+    setActiveCells(changeColor);
+  };
+
+  console.log(activeCells);
 
   return (
     <div className="border-2 relative h-full">
@@ -27,11 +40,19 @@ const Timetable = () => {
         <tbody>
           {Array.from({ length: rows }, (_, rowIndex) => (
             <tr key={rowIndex}>
-              {Array.from({ length: columns }, (_, colIndex) => (
-                <td key={colIndex} className={`border border-gray-300 p-1.5 text-center`}>
-                  {/* Row {rowIndex + 1}, Col {colIndex + 1} */}
-                </td>
-              ))}
+              {Array.from({ length: columns }, (_, colIndex) => {
+                const id = `0${rowIndex}${(colIndex + 1) * 10}`;
+                return (
+                  <td
+                    key={colIndex}
+                    id={id}
+                    className={`border border-gray-300 p-1.5 text-center ${
+                      activeCells[id] ? 'bg-slate-500' : 'bg-white'
+                    }`}
+                    onClick={() => toggleCellColor(id)}
+                  ></td>
+                );
+              })}
             </tr>
           ))}
         </tbody>
