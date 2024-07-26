98629fec8300e4115ad9397af8f5765d67c9b74d
youngsupark1 <eddy364885@gmail.com>
2024-07-26 17:44:34 +0900

    🩹 fix : 클라이언트 레이아웃에 if문은 이제 필요없어서 다시 예전 코드로 되돌렸습니다.

--- src/components/templates/ClientLayout.tsx
+++ src/components/templates/ClientLayout.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { useStore } from '@/stores/sidebar.store';
 import Header from '@/components/molecules/Header';
 import Sidebar from '@/components/molecules/Sidebar';
-import { usePathname } from 'next/navigation';
 
 // Next.js에서는 use client 지시어가 있는 컴포넌트에서 metadata를 내보내는 것이 허용되지 않습니다.
 // 이 문제를 해결하기 위해서는 layout.tsx 파일을 클라이언트 컴포넌트와 서버 컴포넌트로 분리
@@ -13,25 +12,21 @@ import { usePathname } from 'next/navigation';
 const ClientLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { sidebarOpen, toggleSidebar, toggleGrid } = useStore(); // 상태와 상태 변경 함수 가져오기
 
-  if (usePathname() !== '/landing') {
-    return (
-      <div className="flex flex-col min-h-screen">
-        <Header toggleSidebar={toggleSidebar} toggleGrid={toggleGrid} />
-        <div className="flex flex-grow">
-          {sidebarOpen && <Sidebar onClose={toggleSidebar} />}
-          <main
-            className={`bg-gray-400 ${
-              sidebarOpen ? 'w-[764px]' : 'w-full'
-            } h-[930px] flex items-center justify-center transition-all duration-300`}
-          >
-            {children}
-          </main>
-        </div>
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header toggleSidebar={toggleSidebar} toggleGrid={toggleGrid} />
+      <div className="flex flex-grow">
+        {sidebarOpen && <Sidebar onClose={toggleSidebar} />}
+        <main
+          className={`bg-gray-400 ${
+            sidebarOpen ? 'w-[764px]' : 'w-full'
+          } h-[930px] flex items-center justify-center transition-all duration-300`}
+        >
+          {children}
+        </main>
       </div>
-    );
-  } else {
-    return <>{children}</>;
-  }
+    </div>
+  );
 };
 
 export default ClientLayout;
