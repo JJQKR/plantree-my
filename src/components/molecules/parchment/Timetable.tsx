69d4743ce07d6f638beaed2567db6f54f07fa008
Boyoung Lee <genuss040822@gamil.com>
2024-07-26 11:55:39 +0900

    [✨ update] 타임테이블 드래그 기능 추가

--- src/components/molecules/parchment/Timetable.tsx
+++ src/components/molecules/parchment/Timetable.tsx
@@ -8,23 +8,37 @@ type activeCellsObjet = {
 
 const Timetable = () => {
   const [activeCells, setActiveCells] = useState<activeCellsObjet>({});
+  const [isMouseDown, setIsMouseDown] = useState(false);
 
   const rows = 24;
   const columns = 6;
   const minutes = [10, 20, 30, 40, 50, 60];
   const hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 
+  const handleMouseDown = (id: string) => {
+    setIsMouseDown(true);
+    toggleCellColor(id);
+  };
+
+  const handleMouseOver = (id: string) => {
+    if (isMouseDown) {
+      toggleCellColor(id);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+  };
   const toggleCellColor = (id: string) => {
     const changeColor = (prev: activeCellsObjet) => {
       return { ...prev, [id]: !prev[id] };
     };
     setActiveCells(changeColor);
   };
-
   console.log(activeCells);
 
   return (
-    <div className="border-2 relative h-full">
+    <div className="border-2 relative h-full" onMouseUp={handleMouseUp}>
       <h4>Time Table</h4>
       <div className="flex flex-row gap-4 absolute right-1">
         {minutes.map((minute) => {
@@ -41,7 +55,7 @@ const Timetable = () => {
           {Array.from({ length: rows }, (_, rowIndex) => (
             <tr key={rowIndex}>
               {Array.from({ length: columns }, (_, colIndex) => {
-                const id = `0${rowIndex}${(colIndex + 1) * 10}`;
+                const id = `${rowIndex}${(colIndex + 1) * 10}`;
                 return (
                   <td
                     key={colIndex}
@@ -49,7 +63,8 @@ const Timetable = () => {
                     className={`border border-gray-300 p-1.5 text-center ${
                       activeCells[id] ? 'bg-slate-500' : 'bg-white'
                     }`}
-                    onClick={() => toggleCellColor(id)}
+                    onMouseDown={() => handleMouseDown(id)}
+                    onMouseOver={() => handleMouseOver(id)}
                   ></td>
                 );
               })}
