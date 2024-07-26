16a3033ae9e8cade4bd41b12816991a5d96da664
youngsupark1 <eddy364885@gmail.com>
2024-07-26 17:41:59 +0900

    🎉 add: 랜딩페이지의 헤더 메인 푸터 파일 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import LandingFooter from '@/components/molecules/LandingFooter';
+import LandingHeader from '@/components/molecules/LandingHeader';
+import LandingMain from '@/components/molecules/LandingMain';
 import React from 'react';
 
 const page = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <LandingHeader />
+      <LandingMain />
+      <LandingFooter />
+    </div>
+  );
 };
 
 export default page;
