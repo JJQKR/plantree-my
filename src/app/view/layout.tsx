590bbeec5624e27176874051399d569190a633bd
JJQKR <yjcoyjco@naver.com>
2024-07-28 18:37:47 +0900

    [update] view layout.tsx 수정

--- src/app/view/layout.tsx
+++ src/app/view/layout.tsx
@@ -1,7 +1,26 @@
-import React, { PropsWithChildren } from 'react';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+import { ReactQueryClientProvider } from '@/providers/query.provider';
+import Header from '@/components/molecules/Header';
 
-const ViewLayout = ({ children }: PropsWithChildren) => {
-  return <div>{children}</div>;
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Plantree',
+  description: '효율적이고 풍부한 일상의 러닝메이트, 나만의 커스텀 다이어리'
 };
 
-export default ViewLayout;
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ReactQueryClientProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ReactQueryClientProvider>
+  );
+}
