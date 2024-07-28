639332cfc76a315177863ad820df9fc5d87e080c
JJQKR <yjcoyjco@naver.com>
2024-07-29 02:15:58 +0900

    [update] 다이어리 뷰 수정

--- src/components/templates/flipping/index.tsx
+++ src/components/templates/flipping/index.tsx
@@ -6,18 +6,28 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Autoplay } from 'swiper/modules';
 
 const TestPages: React.FC = () => {
-  const pages = Array.from({ length: 5 }, (_, i) => (
-    <SwiperSlide key={i}>
-      <div className="flex h-screen">
-        <div className="flex-1 flex items-center justify-center h-full bg-gray-200 border border-gray-300 shadow-md text-2xl">
-          <h3>Page {i * 2 + 1}</h3>
+  const pages = Array.from({ length: 6 }, (_, i) =>
+    i === 0 ? (
+      <SwiperSlide key={i}>
+        <div className="flex items-center justify-center h-screen bg-white border border-gray-300 shadow-md text-2xl">
+          <div className="h-3/4 w-1/3 flex items-center justify-center bg-gray-200 border border-gray-300 shadow-md">
+            <h3>Page 0</h3>
+          </div>
         </div>
-        <div className="flex-1 flex items-center justify-center h-full bg-gray-200 border border-gray-300 shadow-md text-2xl">
-          <h3>Page {i * 2 + 2}</h3>
+      </SwiperSlide>
+    ) : (
+      <SwiperSlide key={i}>
+        <div className="flex h-screen">
+          <div className="flex-1 flex items-center justify-center h-full bg-gray-200 border border-gray-300 shadow-md text-2xl">
+            <h3>Page {i * 2 - 1}</h3>
+          </div>
+          <div className="flex-1 flex items-center justify-center h-full bg-gray-200 border border-gray-300 shadow-md text-2xl">
+            <h3>Page {i * 2}</h3>
+          </div>
         </div>
-      </div>
-    </SwiperSlide>
-  ));
+      </SwiperSlide>
+    )
+  );
 
   return (
     <div className="container mx-auto p-5 max-w-7xl">
