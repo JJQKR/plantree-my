ae74d6edc25f238bd1a3ec44f41536544a2a24d2
YIS <dtdt203@naver.com>
2024-07-31 21:10:36 +0900

    💄 style : 바텀시트 딜리트버튼 제거
    - 아쉽지만 현재상황상 우선순위 뒤로 미룸

--- src/stores/bottomsheet.store.ts
+++ src/stores/bottomsheet.store.ts
@@ -1,5 +1,4 @@
 import { create } from 'zustand';
-import { useDiaryCoverStore } from '@/stores/diarycover.store';
 
 type BottomSheetState = {
   bottomSheetList: { id: string; title: string; content: string }[];
@@ -8,7 +7,6 @@ type BottomSheetState = {
   setBottomSheetList: (newList: { id: string; title: string; content: string }[]) => void;
   moveCard: (dragIndex: number, hoverIndex: number) => void;
   addPageToBottomSheet: (id: string, title: string, content: string) => void;
-  deletePageFromBottomSheet: (id: string) => void;
 };
 
 const useBottomSheetStore = create<BottomSheetState>((set) => ({
@@ -27,14 +25,7 @@ const useBottomSheetStore = create<BottomSheetState>((set) => ({
   addPageToBottomSheet: (id, title, content) =>
     set((state) => ({
       bottomSheetList: [...state.bottomSheetList, { id, title, content }]
-    })),
-  deletePageFromBottomSheet: (id) => {
-    const deletePage = useDiaryCoverStore.getState().deletePage;
-    deletePage(id); // delete page from the cover store as well
-    set((state) => ({
-      bottomSheetList: state.bottomSheetList.filter((page) => page.id !== id)
-    }));
-  }
+    }))
 }));
 
 export default useBottomSheetStore;
