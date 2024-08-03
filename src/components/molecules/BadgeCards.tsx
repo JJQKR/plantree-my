043211c9cfaba3c94d4b97373fd423e9609a87fd
JJQKR <yjcoyjco@naver.com>
2024-08-03 21:15:31 +0900

    [✨ update ] BadgeCards 조건부 이미지 렌더링 로직 추가

--- src/components/molecules/BadgeCards.tsx
+++ src/components/molecules/BadgeCards.tsx
@@ -7,23 +7,35 @@ import FetchMembershipDays from '@/lib/utils/FetchMembershipDays';
 
 const BadgeCards: React.FC = () => {
   const { diaryCount, membershipDays } = useUserStore((state) => state);
-  const [badgesState, setBadgesState] = useState(totalBadges);
+  const [badgesState, setBadgesState] = useState(
+    totalBadges.map((badgeGroup) => ({
+      ...badgeGroup[1], // 초기값으로 획득되지 않은 배지를 설정
+      content: badgeGroup[1].content.replace('true', 'false')
+    }))
+  );
 
   useEffect(() => {
     const updateBadges = () => {
       setBadgesState((prevBadges) =>
-        prevBadges.map((badgeGroup) =>
-          badgeGroup.map((badge) => {
-            const isObtained =
-              (badge.id.includes('다이어리수집가') && diaryCount >= 3) ||
-              (badge.id.includes('문구점사장님') && diaryCount >= 15) ||
-              (badge.id.includes('안녕하세요') && membershipDays >= 1) ||
-              (badge.id.includes('빨리친해지길바라') && membershipDays >= 7) ||
-              (badge.id.includes('찐친') && membershipDays >= 30);
-
-            return isObtained ? { ...badge, isObtained: true } : badge;
-          })
-        )
+        prevBadges.map((badge) => {
+          let isObtained = false;
+          if (badge.id.includes('다이어리수집가') && diaryCount !== null && diaryCount >= 3) {
+            isObtained = true;
+          } else if (badge.id.includes('문구점사장님') && diaryCount !== null && diaryCount >= 15) {
+            isObtained = true;
+          } else if (badge.id.includes('안녕하세요') && membershipDays !== null && membershipDays >= 1) {
+            isObtained = true;
+          } else if (badge.id.includes('빨리친해지길바라') && membershipDays !== null && membershipDays >= 7) {
+            isObtained = true;
+          } else if (badge.id.includes('찐친') && membershipDays !== null && membershipDays >= 30) {
+            isObtained = true;
+          }
+          return {
+            ...badge,
+            isObtained,
+            content: isObtained ? badge.content.replace('false', 'true') : badge.content.replace('true', 'false')
+          };
+        })
       );
     };
 
@@ -32,17 +44,14 @@ const BadgeCards: React.FC = () => {
 
   return (
     <div className="grid grid-cols-3 gap-2">
-      {badgesState.map((badgeGroup, index) => (
+      {badgesState.map((badge, index) => (
         <div key={index} className="m-5">
-          {badgeGroup.map((badge, idx) => (
-            <Image
-              key={idx}
-              src={badge.isObtained ? badge.content : badge.content.replace('true', 'false')}
-              alt={badge.isObtained ? 'Obtained Badge' : 'Unobtained Badge'}
-              width={100}
-              height={150}
-            />
-          ))}
+          <Image
+            src={badge.content}
+            alt={badge.isObtained ? 'Obtained Badge' : 'Unobtained Badge'}
+            width={100}
+            height={150}
+          />
         </div>
       ))}
       <FetchDiaryCount />
