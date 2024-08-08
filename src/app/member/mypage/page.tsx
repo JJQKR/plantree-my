650f5751db38f780f00059127c5cf8bdb6e22466
JJQKR <yjcoyjco@naver.com>
2024-08-08 19:03:21 +0900

    [� style ] myPage CSS 수정 중

--- src/app/member/mypage/page.tsx
+++ src/app/member/mypage/page.tsx
@@ -48,42 +48,44 @@ const MyPage: React.FC = () => {
   return (
     <>
       <div className="w-[80rem] h-[117.5rem] grid grid-cols-1 justify-items-center ">
-        <div className="my-5 w-[61.25rem] h-[40.13rem] shadow-sm ">
-          <GrowthSummary />
-          <GardenCarousel />
-        </div>
-        <div className="w-[61.25rem] h-[29.56rem] bg-white rounded-[20px] shadow-md">
-          <div className="flex flex-row justify-between mt-4 mb-1">
-            <h2 className="font-semibold ml-10">
-              도전과제 <ObtainedBadgesCount /> / {totalBadges.length}
-            </h2>
-            <button className="font-semibold text-[#008A02] rounded-[5px] mr-10" onClick={handleToggleBadgeModal}>
-              전체 보기
-            </button>
-            {isBadgeModalOpen && <BadgeModal />}
+        <div className="w-[61.26rem] h-[101.19rem]">
+          <div className="mt-10 w-[61.25rem] h-[40.13rem] shadow-sm ">
+            <GrowthSummary />
+            <GardenCarousel />
           </div>
-          <BadgeCollection />
-        </div>
+          <div className="w-[61.25rem] h-[29.56rem] bg-white rounded-[20px] shadow-md">
+            <div className="flex flex-row justify-between mt-4 mb-1">
+              <h2 className="font-semibold ml-10">
+                도전과제 <ObtainedBadgesCount /> / {totalBadges.length}
+              </h2>
+              <button className="font-semibold text-[#008A02] rounded-[5px] mr-10" onClick={handleToggleBadgeModal}>
+                전체 보기
+              </button>
+              {isBadgeModalOpen && <BadgeModal />}
+            </div>
+            <BadgeCollection />
+          </div>
 
-        <NicknameButton>
-          <div className="flex items-center justify-start mt-3 pl-3 p-2 bg-white rounded-[10px] shadow-md w-[600px] h-[50px]">
-            <button onClick={handleToggleNicknameModal}>닉네임 변경</button>
-            {isNicknameModalOpen && <NicknameModal />}
-          </div>
-        </NicknameButton>
-        <AccountBarButton>
-          <div className="flex items-center justify-between mt-3 pl-3 p-2 bg-white rounded-[10px] shadow-md w-[600px] h-[50px]">
-            <button onClick={handleAccountBarModal}>계정 설정</button>
-            <p className="mr-5">{email}</p>
-            {isAccountBarModalOpen && <AccountBarModal />}
-          </div>
-        </AccountBarButton>
-        <WithdrawalButton>
-          <div className="flex items-center justify-start my-3 pl-3 p-2 bg-white rounded-[10px] shadow-md w-[600px] h-[50px]">
-            <button onClick={handleToggleWithdrawalModal}>회원 탈퇴</button>
-            {isWithdrawalModalOpen && <WithdrawalModal />}
-          </div>
-        </WithdrawalButton>
+          <NicknameButton>
+            <div className="flex items-center justify-start mt-3 pl-3 p-2 bg-white rounded-[10px] shadow-md w-[61.25rem] h-[7rem]">
+              <button onClick={handleToggleNicknameModal}>닉네임 변경</button>
+              {isNicknameModalOpen && <NicknameModal />}
+            </div>
+          </NicknameButton>
+          <AccountBarButton>
+            <div className="flex items-center justify-between mt-3 pl-3 p-2 bg-white rounded-[10px] shadow-md w-[61.25rem] h-[7rem]">
+              <button onClick={handleAccountBarModal}>계정 설정</button>
+              <p className="mr-5">{email}</p>
+              {isAccountBarModalOpen && <AccountBarModal />}
+            </div>
+          </AccountBarButton>
+          <WithdrawalButton>
+            <div className="flex items-center justify-start my-3 pl-3 p-2 bg-white rounded-[10px] shadow-md w-[61.25rem] h-[7rem]">
+              <button onClick={handleToggleWithdrawalModal}>회원 탈퇴</button>
+              {isWithdrawalModalOpen && <WithdrawalModal />}
+            </div>
+          </WithdrawalButton>
+        </div>
       </div>
     </>
   );
