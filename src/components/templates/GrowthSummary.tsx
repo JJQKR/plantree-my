8176f2169c8cfd8c1003caae23abe8d1c99a16a7
JJQKR <yjcoyjco@naver.com>
2024-08-08 20:23:34 +0900

    [� style] 닉네임 모달까지 CSS

--- src/components/templates/GrowthSummary.tsx
+++ src/components/templates/GrowthSummary.tsx
@@ -31,13 +31,15 @@ const GrowthSummary = () => {
     <>
       <FetchUserData />
       <FetchMembershipDays />
-      <div className="flex flex-row w-[61.25rem] h-[9.5rem] bg-white rounded-tr-[20px] rounded-tl-[20px]">
-        <div>{levelId && <ProfileStages levelId={levelId} size={90} />}</div>
-        <div className="items-center">
-          <h3 className="font-semibold text-[1rem] text-[#008A02]">{levelName}</h3>
-          <h3 className="font-semibold text-[14px]">{nickname} 님의 정원</h3>
 
-          <div className="mb-4 text-[#727272]">
+      <div className="flex flex-row w-[61.25rem] h-[9.5rem] pt-[1.5rem] pb-[2rem] bg-white rounded-tr-[20px] rounded-tl-[20px]">
+        {/* 여기 pb 왜 지맘대로 먹었다 안 먹었다 하는지 아시는분...? */}
+        <div className="ml-[2rem]">{levelId && <ProfileStages levelId={levelId} size={70} />}</div>
+        <div className="items-center ml-[1rem]">
+          <span className="font-semibold text-[1.63rem] text-[#008A02]">{levelName}</span>
+          <span className="font-semibold text-[1.63rem] ml-[1rem]">{nickname} 님의 정원</span>
+
+          <div className="font-semibold text-[1.38rem] text-[#727272]">
             <p>플랜트리와 함께 {membershipDays}일째,</p>
             <div className="flex justify-between">열심히 나무를 키우고 계시네요!</div>
           </div>
