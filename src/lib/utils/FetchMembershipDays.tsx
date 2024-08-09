8f8eedf369e98dc7a20d5bf4844bdf18938c1e6c
JJQKR <yjcoyjco@naver.com>
2024-08-09 18:03:25 +0900

    [� fix  ]FetchMemberShipDays.tsx auth.getUser로 수정

--- src/lib/utils/FetchMembershipDays.tsx
+++ src/lib/utils/FetchMembershipDays.tsx
@@ -10,12 +10,16 @@ const FetchMembershipDays = () => {
   useEffect(() => {
     const fetchMembershipDays = async (userId: string) => {
       try {
-        const { data, error } = await supabase.from('users').select('created_at').eq('id', userId).maybeSingle();
+        // auth.getUser를 사용하여 auth.users 테이블에서 created_at 값 가져오기
+        const { data, error } = await supabase.auth.getUser(userId);
+
         if (error) throw error;
-        if (data && data.created_at) {
-          const createdAt = new Date(data.created_at);
+
+        if (data && data.user && data.user.created_at) {
+          const createdAtUTC = new Date(data.user.created_at);
+          const createdAtKST = new Date(createdAtUTC.getTime() + 9 * 60 * 60 * 1000); // UTC를 KST로 변환
           const today = new Date();
-          const diffTime = Math.abs(today.getTime() - createdAt.getTime());
+          const diffTime = Math.abs(today.getTime() - createdAtKST.getTime());
           const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
           setMembershipDays(diffDays);
         }
