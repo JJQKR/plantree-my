30c17b56f2e60dc0ac52267d38dc36261a266679
hoondolla <gnswo9187@daum.net>
2024-08-13 21:02:53 +0900

    ✨ update: 프로세스 진행 중 UX 향상 Alert 추가

--- src/components/molecules/LandingFooter.tsx
+++ src/components/molecules/LandingFooter.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const LandingFooter = () => {
   const scrollToTop = () => {
@@ -9,10 +10,10 @@ const LandingFooter = () => {
   };
 
   return (
-    <footer className="w-full bg-green-950 text-white p-8">
-      <div className="w-[80rem] container mx-auto text-center flex flex-col items-center font-bold text-gray-400">
+    <footer className="w-full h-[23rem] bg-green-950 text-white p-8">
+      <div className="w-[80rem] container mx-auto text-center flex flex-col items-center font-bold text-white">
         <div className="flex flex-row mb-8 items-center">
-          <div className="relative w-[20rem] h-[10rem]">
+          <div className="relative w-[20rem] h-[4rem]">
             <Image
               src="/images/footer_logo.png"
               alt="Plantree Logo"
@@ -21,97 +22,67 @@ const LandingFooter = () => {
               sizes="(max-width: 1280px) 100vw, 20rem"
             />
           </div>
-          <div className="text-xl ml-4 mt-7">플랜트리 - 일상을 기록하는 새로운 방법</div>
+          <div className="text-xl ml-4 mt-4">플랜트리 - 일상을 기록하는 새로운 방법</div>
         </div>
 
-        <div className="flex justify-center w-full mb-8 gap-[1.1rem]">
-          <span className="text-lg hover:text-white cursor-pointer" onClick={scrollToTop}>
+        <div className="flex justify-center items-center w-full mb-6 gap-[29rem]">
+          <span className="text-lg hover:text-gray-500 cursor-pointer" onClick={scrollToTop}>
             페이지 최상단으로 이동
           </span>
-          <a href="#" className="text-lg">
+          <Link href="#" className="text-lg">
             Plantree
-          </a>
-          <a href="#" className="text-lg">
+          </Link>
+          <Link href="#" className="text-lg">
             About Us
-          </a>
+          </Link>
         </div>
 
-        <div className="text-center mb-4">
+        <div className="text-left w-[37.5rem] mb-1">
           <p>© 2024 Team Plantree</p>
         </div>
 
-        <div className="grid grid-cols-3 gap-4 max-w-[80rem] mt-5">
+        <div className="grid grid-cols-3 gap-4 max-w-[80rem] mt-1">
           <div className="flex flex-row items-center gap-[1.1rem]">
-            <h2 className="text-lg font-bold">FE</h2>
+            <h2 className="text-xs font-bold">FE</h2>
             <p>이보영</p>
-            <a
-              href="https://github.com/osoon9295"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-red-500 p-1 rounded-lg text-black hover:text-white hover:bg-red-700"
-            >
-              깃허브
-            </a>
+            <Link href="https://github.com/osoon9295" target="_blank" rel="noopener noreferrer">
+              <Image src="/images/githublogo.png" alt="GitHub Logo" width={40} height={40} />
+            </Link>
           </div>
           <div className="flex flex-row items-center gap-[1.1rem]">
             <h2 className="text-lg font-bold">FE</h2>
             <p>강연주</p>
-            <a
-              href="https://github.com/JJQKR"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-red-500 p-1 rounded-lg text-black hover:text-white hover:bg-red-700"
-            >
-              깃허브
-            </a>
+            <Link href="https://github.com/JJQKR" target="_blank" rel="noopener noreferrer">
+              <Image src="/images/githublogo.png" alt="GitHub Logo" width={40} height={40} />
+            </Link>
           </div>
           <div className="flex flex-row items-center gap-[1.1rem]">
             <h2 className="text-lg font-bold">FE</h2>
             <p>김재훈</p>
-            <a
-              href="https://github.com/hoondolla"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-red-500 p-1 rounded-lg text-black hover:text-white hover:bg-red-700"
-            >
-              깃허브
-            </a>
+            <Link href="https://github.com/hoondolla" target="_blank" rel="noopener noreferrer">
+              <Image src="/images/githublogo.png" alt="GitHub Logo" width={40} height={40} />
+            </Link>
           </div>
           <div className="flex flex-row items-center gap-[1.1rem]">
             <h2 className="text-lg font-bold">FE</h2>
             <p>박영수</p>
-            <a
-              href="https://github.com/youngsupark1"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-red-500 p-1 rounded-lg text-black hover:text-white hover:bg-red-700"
-            >
-              깃허브
-            </a>
+            <Link href="https://github.com/youngsupark1" target="_blank" rel="noopener noreferrer">
+              <Image src="/images/githublogo.png" alt="GitHub Logo" width={40} height={40} />
+            </Link>
           </div>
           <div className="flex flex-row items-center gap-[1.1rem]">
             <h2 className="text-lg font-bold">FE</h2>
             <p>유인수</p>
-            <a
-              href="https://github.com/YISYISYISYIS"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-red-500 p-1 rounded-lg text-black hover:text-white hover:bg-red-700"
-            >
-              깃허브
-            </a>
+            <Link href="https://github.com/YISYISYISYIS" target="_blank" rel="noopener noreferrer">
+              <Image src="/images/githublogo.png" alt="GitHub Logo" width={40} height={40} />
+            </Link>
           </div>
           <div className="flex flex-row items-center gap-[1.1rem]">
             <h2 className="text-lg font-bold">DE</h2>
             <p>황민균</p>
-            <a
-              href="https://notion.so"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-red-500 p-1 rounded-lg text-black hover:text-white hover:bg-red-700"
-            >
-              노션 포폴
-            </a>
+            <Link href="https://notion.so" target="_blank" rel="noopener noreferrer">
+              <Image src="/images/githublogo.png" alt="GitHub Logo" width={40} height={40} />
+            </Link>
           </div>
         </div>
       </div>
--- src/components/templates/DiaryCase.tsx
+++ src/components/templates/DiaryCase.tsx
@@ -135,6 +135,12 @@ const DiaryCase: React.FC = () => {
     }
     const diaryId = uuid(); // 새로운 다이어리 ID 생성
     router.push(`/member/diary/${diaryId}/cover`); // 새로운 다이어리 생성 페이지로 이동
+    Swal.fire({
+      title: '다이어리 생성 성공!',
+      text: '다이어리 커스텀 완료 후 저장 버튼을 눌러 다이어리를 저장하세요!',
+      icon: 'success',
+      confirmButtonText: 'OK'
+    });
   };
 
   // 다이어리 클릭 핸들러
@@ -152,7 +158,7 @@ const DiaryCase: React.FC = () => {
       >
         {gridView ? (
           // 그리드 뷰에서 다이어리 커버 표시
-          <div className="grid grid-cols-3 gap-20 max-w-full mt-[100px]">
+          <div className="grid grid-cols-3 gap-20 max-w-full mt-[10rem]">
             {diaryCovers.length > 0 ? (
               diaryCovers.map((cover, index) =>
                 cover.cover_id ? (
@@ -161,8 +167,8 @@ const DiaryCase: React.FC = () => {
                     className="flex flex-col items-center justify-center cursor-pointer"
                     onClick={() => handleDiaryClick(cover.diary_id as string)}
                     style={{
-                      width: '250px',
-                      height: '400px'
+                      width: '25rem',
+                      height: '40rem'
                     }}
                   >
                     <div className="relative flex flex-col items-center justify-center w-full h-full rounded shadow-md overflow-hidden">
@@ -227,7 +233,7 @@ const DiaryCase: React.FC = () => {
                 ) : null
               )
             ) : (
-              <div className="flex items-center justify-center w-[250px] h-[400px] bg-red-300 rounded shadow-md text-2xl font-bold text-black">
+              <div className="flex items-center justify-center w-[25rem] h-[40rem] bg-red-300 rounded shadow-md text-2xl font-bold text-black">
                 다이어리가 없습니다
               </div>
             )}
@@ -256,7 +262,7 @@ const DiaryCase: React.FC = () => {
                   <SwiperSlide
                     key={cover.cover_id}
                     onClick={() => handleDiaryClick(cover.diary_id as string)}
-                    className="relative cursor-pointer flex flex-col w-[480px] h-[720px] items-center justify-center rounded shadow-md text-2xl font-bold text-black"
+                    className="relative cursor-pointer flex flex-col w-[48rem] h-[72rem] items-center justify-center rounded shadow-md text-2xl font-bold text-black"
                     style={{
                       backgroundColor: cover.cover_bg_color,
                       width: cover.cover_stage_size.width * cover.cover_scale,
@@ -323,7 +329,7 @@ const DiaryCase: React.FC = () => {
                 ) : null
               )
             ) : (
-              <SwiperSlide className="flex items-center justify-center w-[480px] h-[720px] bg-red-300 rounded shadow-md text-2xl font-bold text-black">
+              <SwiperSlide className="flex items-center justify-center w-[48rem] h-[72rem] bg-red-300 rounded shadow-md text-2xl font-bold text-black">
                 <button
                   onClick={handleCreateDiary}
                   className="flex flex-col items-center justify-center text-center"
