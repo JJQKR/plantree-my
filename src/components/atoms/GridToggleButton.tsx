ae678c5144492dcfc8485e2f5781b92ae78683d2
youngsupark1 <eddy364885@gmail.com>
2024-08-18 20:48:01 +0900

    🩹 fix  : 사이드바, 그리드 버튼 아이콘 변경

--- src/components/atoms/GridToggleButton.tsx
+++ src/components/atoms/GridToggleButton.tsx
@@ -1,17 +1,17 @@
 import { GridToggleButtonProps } from '@/types/main';
 import React from 'react';
-import { AiFillAppstore, AiFillBook } from 'react-icons/ai';
+import { TiThLarge, TiThLargeOutline } from 'react-icons/ti';
 
 const GridToggleButton: React.FC<GridToggleButtonProps> = ({ onClick, gridView }) => {
   return (
     <button
       onClick={onClick}
-      className="p-2 mr-4 bg-transparent border-none outline-none hover:bg-gray-600 rounded transition duration-300"
+      className="p-2 mr-4 bg-transparent border-none outline-none hover:bg-gray-200 rounded transition duration-300"
     >
       {gridView ? (
-        <AiFillBook className="text-[4rem] text-green-400 sm:text-[2.5rem]" /> // gridView가 true일 때 AiFillBook 아이콘 렌더링
+        <TiThLarge className="text-[4rem] text-green-400 sm:text-[2.5rem]" /> // gridView가 true일 때 AiFillBook 아이콘 렌더링
       ) : (
-        <AiFillAppstore className="text-[4rem] text-green-400 sm:text-[2.5rem]" /> // gridView가 false일 때 AiFillAppstore 아이콘 렌더링
+        <TiThLargeOutline className="text-[4rem] text-green-400 sm:text-[2.5rem]" /> // gridView가 false일 때 AiFillAppstore 아이콘 렌더링
       )}
     </button>
   );
