3841eaaac254bba69fd78cf00ee51a5f602cc3b9
youngsupark1 <eddy364885@gmail.com>
2024-08-18 22:41:23 +0900

    🩹 fix : + 새 다이어리 생성 버튼 CSS 수정

--- src/components/atoms/CreateDiaryButton.tsx
+++ src/components/atoms/CreateDiaryButton.tsx
@@ -8,7 +8,7 @@ const CreateDiaryButton: React.FC<CreateDiaryButtonProps> = ({ onClick }) => {
       onClick={onClick}
       className="flex items-center text-[2.5rem] px-[2rem] py-[0.5rem] sm:px-4 sm:py-4 bg-green-700 text-white font-bold rounded-lg shadow hover:bg-[#006201] transition-all duration-200"
     >
-      <FaPlus className="sm:ml-[0.1rem] sm:text-[2.5rem] " />
+      <FaPlus className="sm:ml-[0.1rem] sm:text-[2.5rem] mr-[1rem]" />
       <span className="sm:hidden inline">새 다이어리 생성</span>
     </button>
   );
