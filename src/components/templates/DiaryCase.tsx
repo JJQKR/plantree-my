5b7cd62b0843beaae4b18349239c1ab4d10f79a5
youngsupark1 <eddy364885@gmail.com>
2024-08-20 22:57:15 +0900

    🩹fix : 모바일 터치 범위 다시 수정

--- src/components/templates/DiaryCase.tsx
+++ src/components/templates/DiaryCase.tsx
@@ -198,10 +198,11 @@ const DiaryCase: React.FC = () => {
                   className="flex flex-col items-center justify-center cursor-pointer"
                   onPointerUp={(e) => {
                     const cardElement = e.currentTarget;
-                    const isSmallScreen = window.innerWidth <= 768; // sm 기준에 따른 조건 추가
-                    const clickThreshold = isSmallScreen ? cardElement.offsetHeight : cardElement.offsetHeight / 1.5; // sm 기준에서는 카드 전체 클릭 허용
+                    const isSmallScreen = window.innerWidth <= 768;
+                    const clickThreshold = isSmallScreen
+                      ? cardElement.offsetHeight / 1.2
+                      : cardElement.offsetHeight / 1.5;
                     if (e.clientY < cardElement.offsetTop + clickThreshold) {
-                      // 상단 절반 클릭 시 혹은 sm 기준에서는 전체 클릭 시
                       handleDiaryClick(cover.diary_id as string);
                     }
                   }}
@@ -307,12 +308,11 @@ const DiaryCase: React.FC = () => {
                       key={cover.cover_id}
                       onPointerUp={(e) => {
                         const cardElement = e.currentTarget;
-                        const isSmallScreen = window.innerWidth <= 768; // sm 기준에 따른 조건 추가
+                        const isSmallScreen = window.innerWidth <= 768;
                         const clickThreshold = isSmallScreen
-                          ? cardElement.offsetHeight
-                          : cardElement.offsetHeight / 1.5; // sm 기준에서는 카드 전체 클릭 허용
+                          ? cardElement.offsetHeight / 1.2
+                          : cardElement.offsetHeight / 1.5;
                         if (e.clientY < cardElement.offsetTop + clickThreshold) {
-                          // 상단 절반 클릭 시 혹은 sm 기준에서는 전체 클릭 시
                           handleDiaryClick(cover.diary_id as string);
                         }
                       }}
