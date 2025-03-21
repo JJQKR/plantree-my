b05a149f1c01c86bf77de872341895c6959f0622
JJQKR <yjcoyjco@naver.com>
2025-03-21 17:30:20 +0900

    [FIX] next.config.mjs 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['xmrsttncmglunfluwmqd.supabase.co'] // 허용할 도메인 추가
+//   }
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['xmrsttncmglunfluwmqd.supabase.co'] // 허용할 도메인 추가
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // canvas를 서버 사이드에서 제외
+      config.externals = [...config.externals, 'canvas'];
+    }
+    return config;
   }
 };
 
